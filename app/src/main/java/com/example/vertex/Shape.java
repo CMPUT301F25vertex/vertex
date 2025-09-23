bffd6caf1d6f80514b87c5f3f4c3874410f5a465
r4nd0mus3rnam3 <180621280+r4nd0mus3rnam3@users.noreply.github.com>
2025-09-22 20:09:09 -0600

    Rename color string in Shape.java

--- app/src/main/java/com/example/vertex/Shape.java
+++ app/src/main/java/com/example/vertex/Shape.java
@@ -3,7 +3,7 @@ package com.example.vertex;
 abstract public class Shape {
     private int x;
     private int y;
-    private String color = "blue";
+    private String color = "different color from blue";
 
     public Shape(int x, int y) {
         this.x = x;
