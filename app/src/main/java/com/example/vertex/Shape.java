3dcebae468b4bf2a4f8e40e90d228a49c4714bf1
Ayaad <ayaad.contact@gmail.com>
2025-09-24 17:18:01 -0600

    change Shape color to red

--- app/src/main/java/com/example/vertex/Shape.java
+++ app/src/main/java/com/example/vertex/Shape.java
@@ -3,7 +3,7 @@ package com.example.vertex;
 abstract public class Shape {
     private int x;
     private int y;
-    private String color = "green";
+    private String color = "red";
 
     public Shape(int x, int y) {
         this.x = x;
