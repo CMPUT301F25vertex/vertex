a21b80bd62251fc44e46fde00be5354677e4f2b6
Ben-Bingham <ben@benbingham.ca>
2025-09-24 18:03:16 -0600

    Added colour to personal branch

--- app/src/main/java/com/example/vertex/Shape.java
+++ app/src/main/java/com/example/vertex/Shape.java
@@ -4,8 +4,11 @@ abstract public class Shape {
     private int x;
     private int y;
 
-    public Shape(int x, int y) {
+    private String colour = "yellow";
+
+    public Shape(int x, int y, String colour) {
         this.x = x;
         this.y = y;
+        this.colour = colour;
     }
 }
